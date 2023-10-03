c0c948c4ca39524441e91f03f222a4e0c2a3b461
IvorStur <90836443+IvorStur@users.noreply.github.com>
2023-10-03 09:26:09 +0200

    2 files modified, 2 added

---
+++ createData.js
@@ -0,0 +1,19 @@
+const fs = require('fs/promises');
+const { faker } = require('@faker-js/faker');
+
+async function f2() {
+  console.log('Data gen');
+
+  const data = [];
+  for (let i = 0; i <= 10; i++) {
+    const name = faker.commerce.productName();
+    const description = faker.commerce.productDescription();
+
+    data.push({ name: `${name}`, description: `${description}` });
+  }
+  var json = JSON.stringify(data);
+
+  await fs.writeFile('data2.json', json, 'utf8');
+}
+
+f2();
